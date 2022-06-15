72a857ee7740537961c4a30396c1c81487f7f5a3
GermanBluefox <dogafox@gmail.com>
2022-06-15 19:47:34 +0200

    Tune builder

--- src/craco.config.js
+++ src/craco.config.js
@@ -2,13 +2,23 @@ const CracoEsbuildPlugin = require('craco-esbuild');
 const { ProvidePlugin } = require('webpack');
 const cracoModuleFederation = require('craco-module-federation');
 
-console.log('craco');
-
 module.exports = {
-    plugins: [{ plugin: CracoEsbuildPlugin }, { plugin: cracoModuleFederation }],
+    plugins: [
+        { plugin: CracoEsbuildPlugin },
+        { plugin: cracoModuleFederation, options: { useNamedChunkIds: true } }
+    ],
+    devServer: {
+        proxy: {
+            '/files': 'http://localhost:8081',
+            '/adapter': 'http://localhost:8081',
+            '/session': 'http://localhost:8081',
+            '/log': 'http://localhost:8081',
+            '/lib': 'http://localhost:8081',
+        }
+    },
     webpack: {
         output: {
-            publicPath: 'auto',
+            publicPath: './',
         },
         plugins: [
             new ProvidePlugin({
@@ -16,7 +26,7 @@ module.exports = {
             }),
         ],
         configure: webpackConfig => {
-            webpackConfig.output.publicPath = 'auto';
+            webpackConfig.output.publicPath = './';
             return webpackConfig;
         },
     },
