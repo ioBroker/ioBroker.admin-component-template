30e030a0878b76e95b3d6efd9ba3a030c3b6e577
GermanBluefox <dogafox@gmail.com>
2026-07-25 23:37:54 +0300

    Breaking: guiApi = 2

---
+++ tasks.ts
@@ -0,0 +1,35 @@
+import { deleteFoldersRecursive, npmInstall, buildReact, copyFiles } from '@iobroker/build-tools';
+
+const src = `${__dirname}/src-admin/`;
+
+function clean(): void {
+    deleteFoldersRecursive(`${__dirname}/admin`, ['admin-component-template.png', 'jsonConfig.json']);
+    deleteFoldersRecursive(`${src}build`);
+}
+
+function copyAllFiles(): void {
+    copyFiles(['src-admin/build/static/js/*.js'], 'admin/custom/static/js');
+    copyFiles(['src-admin/build/static/js/*.map'], 'admin/custom/static/js');
+    copyFiles(['src-admin/build/customComponents.js'], 'admin/custom');
+    copyFiles(['src-admin/build/customComponents.js.map'], 'admin/custom');
+    copyFiles(['src-admin/src/i18n/*.json'], 'admin/custom/i18n');
+}
+
+if (process.argv.includes('--0-clean')) {
+    clean();
+} else if (process.argv.includes('--1-npm')) {
+    npmInstall(src).catch((e: unknown) => console.error(`Cannot install npm: ${e as Error}`));
+} else if (process.argv.includes('--2-build')) {
+    buildReact(src, { vite: true }).catch((e: unknown) => console.error(`Cannot build: ${e as Error}`));
+} else if (process.argv.includes('--3-copy')) {
+    copyAllFiles();
+} else {
+    clean();
+    npmInstall(src)
+        .then(() => buildReact(src, { vite: true }))
+        .then(() => copyAllFiles())
+        .catch((e: unknown) => {
+            console.error(e);
+            process.exit(2);
+        });
+}
